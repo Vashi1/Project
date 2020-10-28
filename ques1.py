fd8fcef11d30f3931b79da0ba567203ef5e61b8f
spaceromero <rakshithdogra@gmail.com>
2020-10-28 11:03:06 +0530

    Question 1

--- ques1.py
+++ ques1.py
@@ -1,2 +1,7 @@
 # Program to read 4 digit number and calculate the sum of square of first 2 digits 
-numb = 
+numb = int(input("Enter the number : "))
+sum_sq = 0
+a = numb % 100
+b = numb // 100
+sum_sq = a ** 2 + b ** 2
+print(sum_sq)
