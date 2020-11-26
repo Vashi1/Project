9018e4a8c86e0e57be3af71fbc6c488a9c679826
Rakshith <rakshithdogra@gmail.com>
2020-11-26 16:01:04 +0530

    V3

--- ques4.py
+++ ques4.py
@@ -1,8 +1,8 @@
 # To check whether a sting is palindrome or not
 string = input("Enter the string : ")
 a = ""
-for i in string:
-    a = a + i
+for i in range(len(string) - 1, -1, -1):
+    a = a + string[i]
 if a == string:
     print("The string ", string, "is a palindrome")
 else:
