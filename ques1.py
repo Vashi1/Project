02f5829bde094e6933b9bad170f487d0f806eaef
Rakshith <rakshithdogra@gmail.com>
2020-11-26 13:54:29 +0530

    main

--- ques1.py
+++ ques1.py
@@ -1,7 +1,12 @@
 # Program to read 4 digit number and calculate the sum of square of first 2 digits 
-numb = int(input("Enter the number : "))
-sum_sq = 0
-a = numb % 100
-b = numb // 100
-sum_sq = a ** 2 + b ** 2
-print(sum_sq)
+numb = input("Enter the number : ")
+# Check length
+if len(numb) == 4:
+    sum_sq = 0
+    numb = int(numb)
+    a = numb % 100
+    b = numb // 100
+    sum_sq = a ** 2 + b ** 2
+    print(sum_sq)
+else:
+    print("Please enter a 4 digit number")
--- ques3.py
+++ ques3.py
@@ -1,4 +1,4 @@
 #Write a Program to to randomly generate a number from 100-1000 both inclusive values
 import random
 numb = random.randint(100, 1000)
-print("Randomly selected Lucky Viewer is : ", numb)
+print('\u0332'.join("Randomly selected Lucky Viewer is : "), numb)
